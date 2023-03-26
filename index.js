1485a546926b2fbe8eb1adc019adfc1738ecddea
kvksatish <kvksatish98@gmail.com>
2023-03-26 15:34:01 +0530

    fehvguae

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
  const express = require("express")
  var jwt = require('jsonwebtoken');
- //const bcrypt = require('bcrypt');
+ const bcrypt = require('bcrypt');
  const { connection } = require("./Config/db.js")
- //const { UserModel } = require("./Models/User")
-// const passwordValidator = require('password-validator');
-// const validator = require('validator');
+ const { UserModel } = require("./Models/User")
+const passwordValidator = require('password-validator');
+const validator = require('validator');
  const cors = require('cors');
-// const { CourseModel } = require("./Models/Course.js");
+ const { CourseModel } = require("./Models/Course.js");
 
 const app = express()
 
@@ -19,115 +19,115 @@ app.get("/", (req, res) => {
 })
 //ytfytfytvtyvyt
 
-// const passwordSchema = new passwordValidator();
+const passwordSchema = new passwordValidator();
 
-// // Define the password criteria
-// passwordSchema
-//     .is().min(8) // Minimum length of 8 characters
-//     .has().uppercase() // Must have at least one uppercase letter
-//     .has().lowercase() // Must have at least one lowercase letter
-//     .has().digits() // Must have at least one digit
-//     .has().symbols() // Must have at least one symbol
-//     .has().not().spaces(); // Must not contain spaces
+// Define the password criteria
+passwordSchema
+    .is().min(8) // Minimum length of 8 characters
+    .has().uppercase() // Must have at least one uppercase letter
+    .has().lowercase() // Must have at least one lowercase letter
+    .has().digits() // Must have at least one digit
+    .has().symbols() // Must have at least one symbol
+    .has().not().spaces(); // Must not contain spaces
 
-// app.post("/signup", async (req, res) => {
-//     console.log(req.body)
-//     let { username, email, password } = req.body
+app.post("/signup", async (req, res) => {
+    console.log(req.body)
+    let { username, email, password } = req.body
 
-//     // Validate the email
-//     if (!validator.isEmail(email)) {
-//         return res.status(400).send("Invalid email address");
-//     }
+    // Validate the email
+    if (!validator.isEmail(email)) {
+        return res.status(400).send("Invalid email address");
+    }
 
-//     // Validate the password
-//     if (!passwordSchema.validate(password)) {
-//         return res.status(400).send("Invalid password. Password must be at least 8 characters long, contain one uppercase letter, one lowercase letter, one digit, one symbol, and no spaces.");
-//     }
+    // Validate the password
+    if (!passwordSchema.validate(password)) {
+        return res.status(400).send("Invalid password. Password must be at least 8 characters long, contain one uppercase letter, one lowercase letter, one digit, one symbol, and no spaces.");
+    }
 
-//     try {
-//         // Hash the password
-//         const hash = await bcrypt.hash(password, 6);
+    try {
+        // Hash the password
+        const hash = await bcrypt.hash(password, 6);
 
-//         // Create a new user with the hashed password
-//         const user = new UserModel({ username, email, password: hash });
+        // Create a new user with the hashed password
+        const user = new UserModel({ username, email, password: hash });
 
-//         // Save the user to the database
-//         await user.save();
+        // Save the user to the database
+        await user.save();
 
-//         res.send("Signup Successful");
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).send("Something went wrong or invalid or used credentials. Please try again later.");
-//     }
-// });
+        res.send("Signup Successful");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Something went wrong or invalid or used credentials. Please try again later.");
+    }
+});
 
-// app.post("/login", async (req, res) => {
-//     let { email, password } = req.body
-//     let user = await UserModel.findOne({ email })
+app.post("/login", async (req, res) => {
+    let { email, password } = req.body
+    let user = await UserModel.findOne({ email })
 
-//     console.log(user)
-//     let hash = user.password
-//     bcrypt.compare(password, hash, function (err, result) {
+    console.log(user)
+    let hash = user.password
+    bcrypt.compare(password, hash, function (err, result) {
 
-//         if (result) {
-//             var token = jwt.sign({ email: email }, 'secret');
-//             console.log(token)
-//             res.send({ "msg": "Login success", "token": token })
+        if (result) {
+            var token = jwt.sign({ email: email }, 'secret');
+            console.log(token)
+            res.send({ "msg": "Login success", "token": token })
 
-//         }
+        }
 
-//         else {
-//             res.send("Login Failed")
-//         }
+        else {
+            res.send("Login Failed")
+        }
 
-//     })
-// })
+    })
+})
 
-// app.get("/dashboard", (req, res) => {
-//     const token = req.headers.authorization.split(" ")[1]
-//     jwt.verify(token, 'secret', async function (err, decoded) {
-//         if (err) {
-//             res.send("Please login")
-//         } else {
-//             let data = await CourseModel.find({ email:decoded.email })
-//             res.send(data)
-//         }
-//     });
+app.get("/dashboard", (req, res) => {
+    const token = req.headers.authorization.split(" ")[1]
+    jwt.verify(token, 'secret', async function (err, decoded) {
+        if (err) {
+            res.send("Please login")
+        } else {
+            let data = await CourseModel.find({ email:decoded.email })
+            res.send(data)
+        }
+    });
 
-// })
-// app.post("/addcourse", (req, res) => {
-//     const token = req.headers.authorization.split(" ")[1]
-//     let { course,
-//         duration,
-//         teacher,
-//         mode,
-//         fee } = req.body
-//         jwt.verify(token, 'secret', async function (err, decoded) {
-//             if (err) {
-//             res.status(500).send("Something went wrong or invalid or used credentials. Please try again later.");
-//         } else {
-//             try {
-//                 const courseData = new CourseModel({
-//                     course,
-//                     duration,
-//                     teacher,
-//                     mode,
-//                     fee,
-//                     email: decoded.email
-//                 });
+})
+app.post("/addcourse", (req, res) => {
+    const token = req.headers.authorization.split(" ")[1]
+    let { course,
+        duration,
+        teacher,
+        mode,
+        fee } = req.body
+        jwt.verify(token, 'secret', async function (err, decoded) {
+            if (err) {
+            res.status(500).send("Something went wrong or invalid or used credentials. Please try again later.");
+        } else {
+            try {
+                const courseData = new CourseModel({
+                    course,
+                    duration,
+                    teacher,
+                    mode,
+                    fee,
+                    email: decoded.email
+                });
 
-//                 // Save the user to the database
-//                 await courseData.save();
+                // Save the user to the database
+                await courseData.save();
 
-//                 res.send("added Successful");
-//             } catch (err) {
-//                 console.error(err);
-//                 res.status(500).send("Something went wrong or invalid or used credentials. Please try again later.");
-//             }
-//         }
-//     });
+                res.send("added Successful");
+            } catch (err) {
+                console.error(err);
+                res.status(500).send("Something went wrong or invalid or used credentials. Please try again later.");
+            }
+        }
+    });
 
-// })
+})
 
 
 app.listen(7500, async () => {
